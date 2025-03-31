bbd93e08a3d35e18485b29e8c6975f106c213dcc
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2025-03-31 13:14:06 +0800

    CGLIB动态代理的实现

--- cglibDynamicProxy/src/main/java/org/example/CglibProxyFactory.java
+++ cglibDynamicProxy/src/main/java/org/example/CglibProxyFactory.java
@@ -4,6 +4,13 @@ import net.sf.cglib.proxy.Enhancer;
 
 public class CglibProxyFactory {
 
+    /**
+     * 这边主要是创建一个Enhancer对象
+     * 把被代理类的类加载器 被代理类 还有方法拦截器作为参数传入
+     * 其中方法拦截器DebugMethodInterceptor实现了接口MethodInterceptor(方法拦截器)
+     * @param clazz
+     * @return
+     */
     public static Object getProxy(Class<?> clazz) {
         // 创建动态代理增强类
         Enhancer enhancer = new Enhancer();
--- cglibDynamicProxy/src/main/java/org/example/DebugMethodInterceptor.java
+++ cglibDynamicProxy/src/main/java/org/example/DebugMethodInterceptor.java
@@ -18,6 +18,21 @@ public class DebugMethodInterceptor implements MethodInterceptor {
      * @param methodProxy 用于调用原始方法
      */
     // 拦截被代理类中的方法
+
+    /**
+     * 重写的MethodInterceptor父类的intercept方法
+     * @param o 被代理的对象 需要增强的对象
+     * @param method 被拦截的方法 需要增强的方法
+     * @param args 方法入参
+     * @param methodProxy 用于调用原始方法
+     * @return
+     * @throws Throwable
+     */
+
+    // 我们使用代理类调用方法时 实际上是调用MethodInterceptor的intercept的方法
+    // MethodInterceptor接口的intercept方法
+    // 被实现类DebugMethodInterceptor增强 这个类可以自定义
+    // 作为参数传到CglibProxyFactory的Enhancer对象里面去
     @Override
     public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         //调用方法之前，我们可以添加自己的操作
